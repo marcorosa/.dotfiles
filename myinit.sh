aacd573fd391592a1224146c920003dd53dc0530
Marco Rosa <marcor165@hotmail.it>
2015-12-10 16:38:08 +0100

    Add zsh support

--- myinit.sh
+++ myinit.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install autojump dropbox git htop ipython silversearcher-ag unrar vim
-sudo apt-get install compizconfig-settings-manager compiz-plugins-extra stow tmux 
-sudo apt-get install sshuttle
+sudo apt-get install -y autojump dropbox git htop ipython silversearcher-ag unrar vim
+# Shell:
+sudo apt-get install -y zsh git-core
+# Graphics:
+sudo apt-get install -y compizconfig-settings-manager compiz-plugins-extra stow tmux 
+# VPN:
+sudo apt-get install -y sshuttle
 #sudo apt-get install ipython3
 
+# notepadqq:
 sudo add-apt-repository ppa:notepadqq-team/notepadqq
 sudo apt-get update
 sudo apt-get install notepadqq
 
+# misc:
 sudo apt-get install cowsay fortune cmatrix
