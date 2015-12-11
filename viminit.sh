d8d5e51ffb5616d7b3e28127a2b7b371f1131c63
Marco <marcor165@hotmail.it>
2015-12-11 17:30:57 +0100

    Added vim plugins

---
+++ viminit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Vundle plugin manager
+git clone https://github.com/VundleVim/Vundle.vim.git ./vim/.vim/bundle/Vundle.vim
+
+# Install other plugins
+vim +PluginInstall +qall
+# YCM
+./vim/.vim/bundle/YouCompleteMe/install.sh
+
