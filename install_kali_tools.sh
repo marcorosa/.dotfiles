50b40187a83427cb3b1931501770d028f5e2a247
Marco Rosa <marcor165@hotmail.it>
2016-09-12 23:11:27 +0200

    Add installation file for additional kali tools

---
+++ install_kali_tools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# IRC
+sudo apt-get install irssi irssi-scripts screen openssh-server
+sudo apt-get install hexchat
+
+# pngtools
+sudo apt-get install pngtools
+
+# Hex editor
+sudo apt-get install bless
