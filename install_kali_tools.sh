c5e6686e64819580a5c10708bc3aa5d37679c975
Marco Rosa <marcor165@hotmail.it>
2016-10-18 11:39:08 +0200

    add auto-accept option (-y)

--- install_kali_tools.sh
+++ install_kali_tools.sh
@@ -3,11 +3,11 @@
 sudo apt-get update
 
 # IRC
-sudo apt-get install irssi irssi-scripts screen openssh-server
-sudo apt-get install hexchat
+sudo apt-get install -y irssi irssi-scripts screen openssh-server
+sudo apt-get install -y hexchat
 
 # pngtools
-sudo apt-get install pngtools
+sudo apt-get install -y pngtools
 
 # Hex editor
-sudo apt-get install bless
+sudo apt-get install -y bless
