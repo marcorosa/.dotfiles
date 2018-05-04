18b89771787a6bd54888bbfaf1252046501ddf59
Marco Rosa <marcor165@hotmail.it>
2018-05-04 16:44:02 +0200

    change order

--- myinit.sh
+++ myinit.sh
@@ -4,15 +4,22 @@ sudo apt update
 
 sudo apt install -y autojump htop silversearcher-ag
 sudo apt install -y vim
-sudo apt install -y unrar
-sudo apt install -y stow tmux
+sudo apt install -y stow tmux unrar
 sudo apt install -y software-properties-common  # Add PPA
 
 # Python utils:
+sudo apt install -y python-dev
 sudo apt install -y python-pip
 pip install -y ipython
 sudo apt install -y python3-pip
 pip3 install -y ipython
+
+# Coding:
+pip install git-up
+sudo apt install -y build-essential cmake
+pip install virtualenv
+sudo apt install -y ruby ruby-dev
+sudo apt install -y make gcc
 
 # Networking:
 pip install -y sshuttle  # VPN
@@ -23,12 +30,6 @@ sudo apt install -y network-manager-pptp-gnome
 sudo apt install -y openssh-client
 #sudo apt-get install -y openssh-server  # For servers
 
-# Coding:
-pip install git-up
-sudo apt install -y build-essential cmake
-sudo apt install -y python-dev
-pip install virtualenv
-
 # Markdown to slides
 sudo apt install nodejs
 sudo apt install npm
@@ -38,7 +39,6 @@ npm install markdown-to-slides -g
 sudo apt install -y compizconfig-settings-manager compiz-plugins-extra
 
 # Jekyll:
-sudo apt install -y ruby ruby-dev make gcc
 sudo gem install jekyll bundler
 
 # Misc:
