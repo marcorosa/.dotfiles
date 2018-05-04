9fc3244642af94927a27236acb1a3c1ee62f491a
Marco Rosa <marcor165@hotmail.it>
2018-05-04 16:51:13 +0200

    remove -y in pip

--- myinit.sh
+++ myinit.sh
@@ -10,9 +10,9 @@ sudo apt install -y software-properties-common  # Add PPA
 # Python utils:
 sudo apt install -y python-dev
 sudo apt install -y python-pip
-pip install -y ipython
+pip install ipython
 sudo apt install -y python3-pip
-pip3 install -y ipython
+pip3 install ipython
 
 # Coding:
 pip install git-up
@@ -22,7 +22,7 @@ sudo apt install -y ruby ruby-dev
 sudo apt install -y make gcc
 
 # Networking:
-pip install -y sshuttle  # VPN
+pip install sshuttle  # VPN
 sudo apt install -y network-manager-openvpn  # openvpn
 sudo apt install -y network-manager-openvpn-gnome
 sudo apt install -y network-manager-pptp  # vpn pptp
